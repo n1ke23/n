e8dc59f0d56a552dbeb713042d2d9a815656cd43
Aleksandr Jashin <67903349+n1ke23@users.noreply.github.com>
2020-10-14 14:09:25 +0300

    добавил тек, кто ходит

--- js.js
+++ js.js
@@ -1,18 +1,21 @@
 window.onload = function () {
   let div = document.querySelector(".game");
-
+  let p = document.querySelector('.text')
   for (let i = 0; i < 9; i++) {
     div.innerHTML += '<div class="cell"></div>';
   }
 
   let move = 0;
+  p.textContent = "Ход игрока, который ставит Х"
 
   div.addEventListener("click", (event) => {
     // console.log(event);
     if (event.target.className == "cell") {
       if (move % 2 == 1) {
+        p.textContent = "Ход игрока, который ставит Х"
         event.target.innerHTML = "o";
       } else {
+        p.textContent = "Ход игрока, который ставит О"
         event.target.innerHTML = "x";
       }
       move++;
@@ -24,102 +27,78 @@ window.onload = function () {
     let allCell = document.getElementsByClassName("cell");
     //console.log(allCell);
     // проверка Х
-    if (
+    if ((
       allCell[0].innerHTML == "x" &&
       allCell[1].innerHTML == "x" &&
       allCell[2].innerHTML == "x"
-    )
-      alert("Победили X");
-    if (
+    ) || (
       allCell[3].innerHTML == "x" &&
       allCell[4].innerHTML == "x" &&
       allCell[5].innerHTML == "x"
-    )
-      alert("Победили X");
-    if (
+      ) || (
       allCell[6].innerHTML == "x" &&
       allCell[7].innerHTML == "x" &&
       allCell[8].innerHTML == "x"
-    )
-      alert("Победили X");
-    if (
+    ) || (
       allCell[0].innerHTML == "x" &&
       allCell[3].innerHTML == "x" &&
       allCell[6].innerHTML == "x"
-    )
-      alert("Победили X");
-    if (
+      ) || (
       allCell[1].innerHTML == "x" &&
       allCell[4].innerHTML == "x" &&
       allCell[7].innerHTML == "x"
-    )
-      alert("Победили X");
-    if (
+      ) || (
       allCell[2].innerHTML == "x" &&
       allCell[5].innerHTML == "x" &&
       allCell[8].innerHTML == "x"
-    )
-      alert("Победили X");
-    if (
+      ) || (
       allCell[0].innerHTML == "x" &&
       allCell[4].innerHTML == "x" &&
       allCell[8].innerHTML == "x"
-    )
-      alert("Победили X");
-    if (
+      ) || (
       allCell[2].innerHTML == "x" &&
       allCell[4].innerHTML == "x" &&
       allCell[6].innerHTML == "x"
-    )
+    )){
       alert("Победили X");
+      location.reload();
+    }
     // проверка О
-    if (
+    if ((
       allCell[0].innerHTML == "o" &&
       allCell[1].innerHTML == "o" &&
       allCell[2].innerHTML == "o"
-    )
-      alert("Победили O");
-    if (
+      ) || (
       allCell[3].innerHTML == "o" &&
       allCell[4].innerHTML == "o" &&
       allCell[5].innerHTML == "o"
-    )
-      alert("Победили O");
-    if (
+      ) || (
       allCell[6].innerHTML == "o" &&
       allCell[7].innerHTML == "o" &&
       allCell[8].innerHTML == "o"
-    )
-      alert("Победили O");
-    if (
+      ) || (
       allCell[0].innerHTML == "o" &&
       allCell[3].innerHTML == "o" &&
       allCell[6].innerHTML == "o"
-    )
-      alert("Победили O");
-    if (
+      ) || (
       allCell[1].innerHTML == "o" &&
       allCell[4].innerHTML == "o" &&
       allCell[7].innerHTML == "o"
-    )
-      alert("Победили O");
-    if (
+      ) || (
       allCell[2].innerHTML == "o" &&
       allCell[5].innerHTML == "o" &&
       allCell[8].innerHTML == "o"
-    )
-      alert("Победили O");
-    if (
+      ) || (
       allCell[0].innerHTML == "o" &&
       allCell[4].innerHTML == "o" &&
       allCell[8].innerHTML == "o"
-    )
-      alert("Победили O");
-    if (
+      ) || (
       allCell[2].innerHTML == "o" &&
       allCell[4].innerHTML == "o" &&
       allCell[6].innerHTML == "o"
-    )
+    )){
       alert("Победили O");
+      location.reload();
+    }
   };
 };
